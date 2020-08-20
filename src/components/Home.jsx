78797cf70c26d8b2834b628234865896a557d5b2
sandaniUma <sanuma276@gmail.com>
2020-08-20 23:35:59 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,8 +11,7 @@ class Home extends Component{
         return(
             <div className = "homePage_parentDiv">
                 <div className = "header">
-                    <Header /> 
-                    
+                    <Header />
                 </div>
                 <div className = "slider">
                     <MainSlider />
