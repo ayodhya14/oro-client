4d7037bf25e54ac5b5f47ad2c251fd2ad1c33085
San <67090049+sandaniUma@users.noreply.github.com>
2020-08-23 01:30:29 +0530

    Modify ProductList in Homepage

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -8,29 +8,63 @@ class Productlist extends Component{
     render() {
         return (
             <div className = "productListParentClass">
+                
+                {/* dummy data 1st row */}
                 <Row>
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card" style={{width: "18rem", marginLeft: "5%"}}>
                     <div className="card-body">
                         <h5 className="card-title">Necklace</h5>
                          <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
                          <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
                      </div>
                  </div>
-                 <div className="card" style={{width: "18rem"}}>
+                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
                     <div className="card-body">
                         <h5 className="card-title">Necklace</h5>
                          <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
                          <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
                      </div>
                  </div>
-                 <div className="card" style={{width: "18rem"}}>
+                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
                     <div className="card-body">
                         <h5 className="card-title">Necklace</h5>
                          <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
                          <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
                      </div>
                  </div>
-                 <div className="card" style={{width: "18rem"}}>
+                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Necklace</h5>
+                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
+                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
+                     </div>
+                 </div>
+               </Row>
+               
+               {/* dummy data 2nd row */}
+               <Row style= {{marginTop: "4vh"}}>
+                <div className="card" style={{width: "18rem", marginLeft: "5%"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Necklace</h5>
+                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
+                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
+                     </div>
+                 </div>
+                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Necklace</h5>
+                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
+                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
+                     </div>
+                 </div>
+                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Necklace</h5>
+                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
+                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
+                     </div>
+                 </div>
+                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
                     <div className="card-body">
                         <h5 className="card-title">Necklace</h5>
                          <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
