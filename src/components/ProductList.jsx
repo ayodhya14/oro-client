e864da5bdb1bc77e842c9ab13996658e4520da22
ayodhya14 <44439503+ayodhya14@users.noreply.github.com>
2020-08-23 14:08:06 +0530

    Getting products using API to home page

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,81 +1,50 @@
 import React, { Component } from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Product from './Product';
+import axios from "axios";
 // import { FaCartPlus } from "react-icons/fa";
 
 import "./ProductList.scss";
-class Productlist extends Component{
+class ProductList extends Component{
+
+    state = {
+        allProducts: [],
+    };
 
     render() {
         return (
             <div className = "productListParentClass">
-                
-                {/* dummy data 1st row */}
-                <Row>
-                <div className="card" style={{width: "18rem", marginLeft: "5%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary"  style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-               </Row>
-               
-               {/* dummy data 2nd row */}
-               <Row style= {{marginTop: "4vh"}}>
-                <div className="card" style={{width: "18rem", marginLeft: "5%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-                 <div className="card" style={{width: "18rem", marginLeft: "2%"}}>
-                    <div className="card-body">
-                        <h5 className="card-title">Necklace</h5>
-                         <img className="card-img" src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg" alt="Neckalace Collection"/>
-                         <a href="#" className="btn btn-primary" style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
-               </Row>
+                <div className="row">
+                    {this.state.allProducts.map((product) => (
+                        <div className="col" key={product.id}>
+                            <Product
+                                key={product.id} 
+                                product={product}
+                            />
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
 
+    async componentDidMount() {
+        let { data } = await axios.get("http://localhost:5000/api/products");
+        console.log(data);
+    
+        let products = data.map((product) => {
+          return {
+            id: product._id,
+            productType: product.productType,
+            imageUrl: product.imageUrl,
+            description: product.description,
+            availableQty: product.availableQty,
+          };
+        });
+    
+        this.setState({ allProducts: products });
+      }
+
 }
-export default Productlist;
+export default ProductList;
