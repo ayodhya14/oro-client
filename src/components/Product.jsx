a23e9d4db1969cf21103e2576092449212ac02e2
San <67090049+sandaniUma@users.noreply.github.com>
2020-08-30 14:40:54 +0530

    Modify Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -10,13 +10,13 @@ class Product extends Component {
             <div className="card" style={{width: "18rem", marginLeft: "5%"}}>
                     <div className="card-body">
                        
-                         <img className="card-img" src={this.props.product.imageUrl} alt="Neckalace Collection"/>
+                         <img className="card-img" src={this.props.product.imageUrl} alt="Jewellery Collection"/>
                          {/* <h5 className="card-title">Necklace</h5> */}
                          <p>Gold Necklace with Flower Petal Shapes as a Pendant set with White Stones.</p>
                          <h6 className="card-title">Rs.32,000</h6>
                          <a href="#" className="btn btn-primary"  style={{width: "15.4rem"}}>Add to Cart</a>
-                     </div>
-                 </div>
+                    </div>
+             </div>
         );
     }
 }
