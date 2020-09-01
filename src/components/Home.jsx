64fb411dba44bbcb011edda124abdf49470f8eb2
ayodhya14 <44439503+ayodhya14@users.noreply.github.com>
2020-09-01 23:20:05 +0530

    Update Home.jsx
    Remove unwanted test files I added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import MainSlider from "./MainSlider";
 import ProductList from "./ProductList";
-import Cart from "./Cart";
-import GoogleBtn from './GoogleBtn';
-import Login from './Login';
-import Logout from './Logout';
+import Cart from "./Cart"; 
 
 import "./Home.scss";
 
@@ -17,20 +14,17 @@ class Home extends Component{
         return(
             <div className = "homePage_parentDiv">
                 <div className = "mainHeaderClass">
-                    <Header />
-                    {/* <Logout /> */}
-                    {/* <Login /> */}
+                    <Header />           
                 </div>
                 <div className = "mainSliderClass">
                     <MainSlider />
                 </div>
                 <div className = "mainProductListClass">
                     <ProductList />
-                    {/* <GoogleBtn /> */}
                 </div>
-                {/* <div className = "viewCartClass">
+                <div className = "viewCartClass">
                     <Cart />
-                </div> */}
+                </div>
                 <div className = "mainFooterClass">
                     <Footer />
                 </div>
@@ -42,24 +36,5 @@ class Home extends Component{
     }
 
 }
-//End---Create a Component---
 
 export default Home;
-
-// import React from 'react';
-// import { Link } from 'react-router';
-
-// class List extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <p>Please choose a repository from the list below.</p>
-//                 <ul>
-//                     <li><Link to="/Home">React</Link></li>
-//                 </ul>
-//             </div>
-//         );
-//     }
-// }
-
-// export default List;
