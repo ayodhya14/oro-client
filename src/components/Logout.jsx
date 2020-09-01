59f8d25db9b246e214fe2f4ea0511ecda6745741
ayodhya14 <44439503+ayodhya14@users.noreply.github.com>
2020-09-01 23:29:08 +0530

    Update Logout.jsx
    Logout working fine

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -3,21 +3,28 @@ import { GoogleLogout } from 'react-google-login';
 
 const clientId = '96012022962-kl09sfnr1noq3q1lt93g3i7bmmrr8n2p.apps.googleusercontent.com';
 
-function Logout() {
-    const onSuccess = () => {
-        alert ("Logout made successfully");
-    };
+class Logout extends Component {
 
-    return (
-        <div className="row" style={{float: "right", marginRight: "2%"}}>
-            <GoogleLogout
-            clientId={clientId}
-            buttonText="Logout"
-            onLogoutSuccess={onSuccess}
-            >
-            </GoogleLogout>
-        </div>
-    );
+    render() {
+        const onSuccess = () => {
+            window.location.reload(true);
+        };
+
+        return (
+            <div className="row" style={{ float: "right", marginRight: "2%" }}>
+                <GoogleLogout
+                    clientId={clientId}
+                    buttonText="Logout"
+                    onLogoutSuccess={onSuccess}
+                >
+
+                </GoogleLogout>
+            </div>
+        );
+
+    }
+
 }
+
 
 export default Logout;
