4c0cca95231fb1917b235f128fd5fbe5839cf25c
Udara Ranatunga <udaraRanatunga@users.noreply.github.com>
2020-09-02 07:55:58 +0530

    Update ProductSearch.jsx

--- src/components/ProductSearch.jsx
+++ src/components/ProductSearch.jsx
@@ -58,45 +58,14 @@ class ProductSearch extends Component{
       }
       
       filterCategory(categoryName) {
-        //Assing all product to new variable
         let filterProduct;
         filterProduct = this.state.filterallProducts;
-        //Filter new variable 
-        if (categoryName === "Bangle") {
-            filterProduct = filterProduct.filter(function (item) {
-                return item.productType === 'Bangle';
-            }).map(function ({ id, imageUrl, description, availableQty, unitPrice, name }) {
-                return { id, imageUrl, description, availableQty, unitPrice, name };
-            });
-
-        } if (categoryName === "Necklace") {
-            filterProduct = filterProduct.filter(function (item) {
-                return item.productType === 'Necklace';
-            }).map(function ({ id, imageUrl, description, availableQty, unitPrice, name }) {
-                return { id, imageUrl, description, availableQty, unitPrice, name };
-            });
-
-        } if (categoryName === "Earring") {
-            filterProduct = filterProduct.filter(function (item) {
-                return item.productType === 'Earring';
-            }).map(function ({ id, imageUrl, description, availableQty, unitPrice, name }) {
-                return { id, imageUrl, description, availableQty, unitPrice, name };
-            });
-
-        } if (categoryName === "Pendent") {
-            filterProduct = filterProduct.filter(function (item) {
-                return item.productType === 'Pendent';
-            }).map(function ({ id, imageUrl, description, availableQty, unitPrice, name }) {
-                return { id, imageUrl, description, availableQty, unitPrice, name };
-            });
-        }
+        filterProduct = filterProduct.filter(function (item) {
+            return item.productType === categoryName;
+        }).map(function ({ id, imageUrl, description, availableQty, unitPrice, name }) {
+            return { id, imageUrl, description, availableQty, unitPrice, name };
+        });
     }
-
-    //   async searchProduct(){
-    //     console.log(this.props.name);
-    //     let product = await axios.get(`http://localhost:5000/api/products/name/${this.props.name}`);
-    //     console.log(product);
-    //   }
 
 }
 export default ProductSearch;
