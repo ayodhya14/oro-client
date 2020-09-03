bfae3d32444feff66c20c2c2b356ef7f46f244f4
San <67090049+sandaniUma@users.noreply.github.com>
2020-09-03 20:24:02 +0530

    Call SingleProduct Page when clicking product name and image

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -21,9 +21,13 @@ class Product extends Component {
             <div className="card" style={{height: "95%", marginBottom: "20px"}}>
                 <div className="card-body">
                     <h5 className="card-title">{this.props.product.productType}</h5>
-                    <img className="card-img productImg" src={this.props.product.imageUrl} alt="Product Image"/>
+                    <a href = "/SingleProduct">
+                         <img className="card-img productImg" src={this.props.product.imageUrl} alt="Product Image"/>
+                    </a>
                     <div className="centerClass">
-                        <h5 className="card-title">{this.props.product.name}</h5>
+                        <a href = "/SingleProduct">
+                            <h5 className="cardProductName">{this.props.product.name}</h5>
+                         </a> 
                     </div>
                     <div className="centerClass">
                         <h6 className="card-title">{this.props.product.unitPrice}</h6>
